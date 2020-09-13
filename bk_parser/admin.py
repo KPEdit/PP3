21bf12d8d28e50bd9ed031b66e5c11adfb099539
jordamo <67597739+jordamo@users.noreply.github.com>
2020-09-13 22:17:15 +0300

    SC

--- bk_parser/admin.py
+++ bk_parser/admin.py
@@ -1,3 +1,63 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+from django.contrib.auth.models import User, Group
+from django.contrib.auth.forms import UserCreationForm, UserChangeForm
+from django import forms
+from django.core.exceptions import ValidationError
+from . import models
 
-# Register your models here.
+
+class BKUserCreationForm(UserCreationForm):
+
+    class Meta:
+        model = models.BKUser
+
+        fields = (
+            'password1',
+            'password2',
+            'username',
+            'name',
+            'last_name',
+            'email',
+            'password'
+        )
+        widgets = {
+            'password': forms.PasswordInput(),
+            'password1': forms.PasswordInput(),
+            'password2': forms.PasswordInput(),
+        }
+
+
+class BKUserChangeForm(UserChangeForm):
+
+    class Meta:
+        model = models.BKUser
+        fields = '__all__'
+
+
+class UserAdmin(BaseUserAdmin):
+    form = BKUserChangeForm
+    add_form = BKUserCreationForm
+
+    list_display = ('email', 'last_name', 'name', 'is_admin')
+    list_filter = ('is_admin',)
+
+    fieldsets = (
+        (None, {'fields': ('username', 'email', 'password', )}),
+        ('Personal info', {'fields': ('name', 'last_name')}),
+        ('Money', {'fields': ('balance',)}),
+    )
+
+    add_fieldsets = (
+        (None, {
+            'classes': ('wide',),
+            'fields': ('email', 'username', 'password1', 'password2', 'name', 'last_name'),
+        }),
+    )
+    search_fields = ('username', 'last_name', 'name', 'email')
+    ordering = ('last_name', 'name',)
+    filter_horizontal = ()
+
+
+admin.site.unregister(Group)
+admin.site.register(models.BKUser, UserAdmin)
--- bk_parser/models.py
+++ bk_parser/models.py
@@ -1,3 +1,87 @@
+from django import forms
 from django.db import models
-
+from django.contrib.auth import models as umodels
+from django.conf import settings
+# from django.contrib.auth.backends import BaseBackend
+from django.contrib.auth.hashers import check_password
 # Create your models here.
+
+
+class BKUserManager(umodels.BaseUserManager):
+
+    def create_user(self, password, username, name, last_name, email):
+        user = self.model(
+            username=username,
+            name=name,
+            last_name=last_name,
+            email=self.normalize_email(email)
+        )
+
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_superuser(self, password, username, name, last_name, email):
+        user = self.create_user(
+            password=password,
+            username=username,
+            name=name,
+            last_name=last_name,
+            email=email
+        )
+        user.is_superuser = True
+        user.is_admin = True
+        user.save(using=self._db)
+        return user
+
+
+class BKUser(umodels.AbstractBaseUser):
+
+    class Meta:
+        db_table = 'bk_user'
+        verbose_name = "BKUser"
+        verbose_name_plural = "BKUsers"
+
+    objects = BKUserManager()
+
+    username = models.CharField(
+        max_length=60,
+        unique=True
+    )
+    name = models.CharField(
+        max_length=256
+    )
+    last_name = models.CharField(
+        max_length=256
+    )
+    email = models.EmailField(
+        verbose_name='Email',
+        unique=True
+    )
+
+    balance = models.FloatField(
+        auto_created=True,
+        default=0.0,
+    )
+
+    def __str__(self):
+        return f"{self.last_name} {self.name}"
+
+    def has_perm(self, app_label):
+        return self.is_superuser
+
+    @property
+    def is_staff(self):
+        return self.is_admin
+
+    def has_module_perms(self, app_label):
+        return self.is_superuser
+
+    is_active = models.BooleanField(default=True)
+    is_admin = models.BooleanField(default=False)
+    is_superuser = models.BooleanField(default=False)
+
+    USERNAME_FIELD = 'username'
+    REQUIRED_FIELDS = ['name', 'last_name', 'email','password',]
+
+
